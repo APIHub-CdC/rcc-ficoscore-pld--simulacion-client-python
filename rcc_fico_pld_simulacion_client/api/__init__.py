61b17c23df8c3a995dac7af5fd776dbb440ee0b6
Daniel Ramon Zapata <dramonz@ciruclodecredito.com.mx>
2022-06-03 16:33:10 -0500

    first commit

---
+++ rcc_fico_pld_simulacion_client/api/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+# import apis into api package
+from rcc_fico_pld_simulacion_client.api.reporte_de_crdito_consolidado_fico_score_y_pld_api import ReporteDeCrditoConsolidadoFICOScoreYPLDApi
